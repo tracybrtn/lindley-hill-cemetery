e804e9f82f63502e3ff46d24314a8eb5cb163497
tracybrtn <tracynburton@gmail.com>
2023-09-25 21:52:30 +0200

    basic client/folder structure created. NPM packages installed -concurently, express, mongoose

---
+++ server/config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lindley-hill-cemetery', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  });
+
+module.exports = mongoose.connection;
